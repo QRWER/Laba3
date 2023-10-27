618ad8bc58955cef7476afde590c078c6da9701f
QRWER <mum500678@gmail.com>
2023-10-27 22:06:04 +0300

    Good version

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import functions.*;
 public class Main {
     public static void main(String[] args) {
-       double[] value = {1, 2, 3, 4, 5};
-       TabulatedFunction obj = new TabulatedFunction(5, 7, value);
-       obj.addPoint(new FunctionPoint(1.5,4));
-       obj.out();
+        try {
+            double[] value = {1, 2, 3, 4, 5};
+            TabulatedFunction obj = new TabulatedFunction(5, 5, value);
+            obj.addPoint(new FunctionPoint(1.5, 4));
+            obj.out();
+        }
+        catch (Exception ex){
+            System.out.println("Exception!!!");
+        }
     }
 }
