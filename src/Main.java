e88ab32b31f03ef1f0c2ae528856eb5153be82f4
QRWER <mum500678@gmail.com>
2023-10-27 22:10:48 +0300

    Good version 2.0

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
             double[] value = {1, 2, 3, 4, 5};
             TabulatedFunction obj = new TabulatedFunction(5, 5, value);
             obj.addPoint(new FunctionPoint(1.5, 4));
-            obj.out();
         }
         catch (Exception ex){
             System.out.println("Exception!!!");
