a518584716bf1055d129840c1377768433b9999e
QRWER <mum500678@gmail.com>
2023-10-30 15:13:29 +0300

    Completed laba 3 v1.0

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@ import functions.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            double[] value = {1, 2, 3, 4, 5};
-            TabulatedFunction obj = new TabulatedFunction(5, 5, value);
-            obj.addPoint(new FunctionPoint(1.5, 4));
+            double[] values = {1, 2, 3, 4, 5};
+            TabulatedFunction test = new LinkedListTabulatedFunction(0, 4, values);
+            test.addPoint(new FunctionPoint(5, 1));
+            test.deletePoint(0);
+            test.setPoint(0, new FunctionPoint(0, 0));
+            for(int i = 0; i<test.getPointsCount(); i++) {
+                System.out.println(test.getPointX(i));
+            }
         }
         catch (Exception ex){
-            System.out.println("Exception!!!");
+            ex.printStackTrace();
         }
     }
 }
--- src/functions/TabulatedFunction.java
+++ src/functions/TabulatedFunction.java
@@ -1,156 +1,29 @@
 package functions;
 
-import functions.InappropriateFunctionPointException;
-import functions.FunctionPointIndexOutOfBoundsException;
+public interface TabulatedFunction {
 
-public class TabulatedFunction {
-    private FunctionPoint[] array;
-    private int length;
-    public TabulatedFunction(double leftX, double rightX, int pointsCount){
-        if (leftX >= rightX || pointsCount<2) throw new IllegalArgumentException("Illegal argument");
-        array = new FunctionPoint[pointsCount+5];
-        length = pointsCount;
-        double j = (rightX-leftX)/(pointsCount-1);
-        for (int i = 0;i<pointsCount;i++){
-            array[i] = new FunctionPoint(leftX,0);
-            leftX+=j;
-        }
-    }
-    public TabulatedFunction(double leftX, double rightX, double[] values){
-        if (leftX >= rightX || values.length<2) throw new IllegalArgumentException("Illegal argument");
-        array = new FunctionPoint[values.length+5];
-        length = values.length;
-        double j = (rightX-leftX)/(values.length-1);
-        for (int i = 0;i< values.length;i++){
-            array[i] = new FunctionPoint(leftX, values[i]);
-            leftX+=j;
-        }
-    }
-    public double getLeftDomainBorder(){
-        return array[0].getY();
-    }
-    public double getRightDomainBorder(){
-        return array[length-1].getY();
-    }
-    public double getFunctionValue(double x){
-        if(x>=array[0].getX() & x<=array[length-1].getX()){
-            double x1 = array[0].getX();
-            double x2 = array[length-1].getX();
-            double y1 = getLeftDomainBorder();
-            double y2 = getRightDomainBorder();
-            return (((x-x1)*(y2-y1))/(x2-x1))-y1;
-        }
-        else
-            return Double.NaN;
-    }
-    public int getPointsCount(){
-        return length;
-    }
-    public FunctionPoint getPoint(int index){
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        FunctionPoint tmp = new FunctionPoint(array[index].getX(), array[index].getY());
-        return tmp;
-    }
-    public void setPoint(int index, FunctionPoint point) throws InappropriateFunctionPointException {
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        if(index == 0){
-            if(point.getX() <= array[index+1].getX()) {
-                array[index] = new FunctionPoint(point);
-            }
-            else {
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-        else if (index == length-1){
-            if(point.getX()>=array[index-1].getX()){
-                array[index] = new FunctionPoint(point);
-            }
-            else{
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-        else{
-            if(point.getX() >= this.array[index - 1].getX() && point.getX() <= this.array[index + 1].getX()){
-                array[index] = new FunctionPoint(point);
-            }
-            else{
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-    }
-    public double getPointX(int index){
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        return array[index].getX();
-    }
-    public void setPointX(int index, double x) throws InappropriateFunctionPointException{
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        if(index == 0){
-            if(x <= array[index+1].getX()) {
-                array[index].setX(x);
-            }
-            else {
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-        else if (index == length-1){
-            if(x >= array[index-1].getX()){
-                array[index].setX(x);
-            }
-            else{
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-        else{
-            if(x >= this.array[index - 1].getX() && x <= this.array[index + 1].getX()){
-                array[index].setX(x);
-            }
-            else{
-                throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-    }
-    public double getPointY(int index){
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        return array[index].getY();
-    }
-    public void setPointY(int index, double y) {
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        array[index].setY(y);
-    }
-    public void deletePoint(int index){
-        if(index<0 || index>=length) throw new FunctionPointIndexOutOfBoundsException("Index out of range");
-        if(length<3) throw new IllegalStateException("Illegal argument");
-        System.arraycopy(array, index+1, array, index, length-index-1);
-        length--;
-    }
-    public void addPoint(FunctionPoint point) throws InappropriateFunctionPointException{
-        int index;
-        if(point.getX() <= array[length-1].getX()){
-            for(index = 0; point.getX() >= array[index].getX(); index++){
-                if(array[index].getX() == point.getX()) throw new InappropriateFunctionPointException("Illegal argument");
-            }
-        }
-        else{
-            index = length;
-        }
-        if(length != array.length){
-            System.arraycopy(array, index, array, index+1, length-index);
-            array[index] = new FunctionPoint(point);
-        }
-        else {
-            FunctionPoint[] NewArray = new FunctionPoint[length+6];
-            System.arraycopy(array, 0, NewArray, 0, index);
-            NewArray[index] = new FunctionPoint(point);
-            System.arraycopy(array, index, NewArray, index+1, length-index);
-            array=NewArray;
-        }
-        length++;
-    }
-    public void out() {
-        for (int i = 0; i < length; i++) {
-            System.out.print(array[i].getX() + " ");
-            System.out.print(array[i].getY());
-            System.out.println();
-        }
-    }
+    double getLeftDomainBorder();
+
+    double getRightDomainBorder();
+
+    double getFunctionValue(double x);
+
+    int getPointsCount();
+
+    FunctionPoint getPoint(int index);
+
+    void setPoint(int index, FunctionPoint point) throws InappropriateFunctionPointException;
+
+    double getPointX(int index);
+
+    void setPointX(int index, double x) throws InappropriateFunctionPointException;
+
+    double getPointY(int index);
+
+    void setPointY(int index, double y);
+
+    void deletePoint(int index);
+
+    void addPoint(FunctionPoint point) throws InappropriateFunctionPointException;
+
 }
